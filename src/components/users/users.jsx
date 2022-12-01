05eb4c246d6e6a994df30bb9c7a55769887fb486
Fatality6 <83357001+Fatality6@users.noreply.github.com>
2022-12-01 13:09:43 +0300

    Lesson 53 Классовые компоненты
    Преобразовал функциональную компоненту Users в классовую и выполнил get запрос при создании класса. В итоге запрос происходит единожды при создании класса.

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -3,41 +3,42 @@ import React from 'react';
 import style from "./users.module.css";
 import photoUser from './../../img/user.png';
 
-const Users = (props) => {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    .then(response => {this.props.setUsers(response.data.items)})
+  }
 
-  if (props.users.length < 4) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      props.setUsers(response.data.items);
-    })
-  } /* временная заглушка */
-
-  return (
-    props.users.map(u =>
-      <div className={style.user} key={u.id}>
-        <div className={style.ava}>
-          <div className={style.img}>
-            <img src={u.photos.small ? u.photos.small : photoUser} alt='' />
+  render() {
+    return (
+      this.props.users.map(u =>
+        <div className={style.user} key={u.id}>
+          <div className={style.ava}>
+            <div className={style.img}>
+              <img src={u.photos.small ? u.photos.small : photoUser} alt='' />
+            </div>
+            <div className={style.btn}>
+              {u.followed
+                ? <button onClick={() => { this.props.unfollow(u.id) }}>удалить</button>
+                : <button onClick={() => { this.props.follow(u.id) }}>добавить</button>
+              }
+            </div>
           </div>
-          <div className={style.btn}>
-            {u.followed
-              ? <button onClick={() => { props.unfollow(u.id) }}>удалить</button>
-              : <button onClick={() => { props.follow(u.id) }}>добавить</button>
-            }
+          <div className={style.info}>
+            <div className={style.leftText}>
+              <div> {u.name} </div>
+              <div> {u.status === null ? 'Нет статуса' : u.status} </div>
+            </div>
+            <div className={style.rightText}>
+              <div> {'город не указан'} </div>
+              <div> {'страна не указана'} </div>
+            </div>
           </div>
         </div>
-        <div className={style.info}>
-          <div className={style.leftText}>
-            <div> {u.name} </div>
-            <div> {u.status === null ? 'Нет статуса' : u.status} </div>
-          </div>
-          <div className={style.rightText}>
-            <div> {'город не указан'} </div>
-            <div> {'страна не указана'} </div>
-          </div>
-        </div>
-      </div>
-    )
-  );
+      )
+    );
+  }
 }
 
 export default Users;
